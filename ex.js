ce1555da3b623c9ace44f7f84dc8557ec3969b04
thuvakrish100 <thuvakrish100@gmail.com>
2017-12-04 23:12:56 +0530

    files initiated

---
+++ ex.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+<script>
+function calSquare(num){
+return num*num;
+}
+document.getElementById("squ").innerHTML= calSquare(num)
+</script>
+</body>
+
+</html>
---
+++ marks.js/marks.js
@@ -0,0 +1,38 @@
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+<p id="marks"></p>
+  <script>
+
+var=marks = 85;
+if (marks>=75){
+marks="A";
+}
+
+else if (marks>=65){
+marks="B";
+}
+
+else if (marks>=55){
+marks="C";
+}
+
+
+else if (marks>=45){
+marks="S";
+}
+
+else
+marks="FAIL";
+}
+
+
+
+document.getElementById("marks").innerHTML= marks();
+
+
+</script>
+
+</body>
+</html>
